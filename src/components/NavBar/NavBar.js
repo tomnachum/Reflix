a8ab734d6332d753f0eaa172b2dd3b555487f7db
tom-nachum <tom.nachum@gmail.com>
2022-11-09 17:14:23 +0200

    desined nav bar component

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,9 +1,20 @@
 import React, { Component } from "react";
 import "./NavBar.css";
+import { Link } from "react-router-dom";
 
 class NavBar extends Component {
   render() {
-    return <div>NavBar</div>;
+    return (
+      <div className="nav-bar">
+        <div className="links">
+          <Link to="/">Home</Link>
+          <Link to="/catalog">Catalog</Link>
+        </div>
+        <div className="image-container">
+          <img src="https://portalrefil.com.br/wp-content/uploads/2021/07/logo-reflix-quadrado.png" />
+        </div>
+      </div>
+    );
   }
 }
 
