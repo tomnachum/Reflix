a6126a5bd752002ce99b8ede3e70559c886d1a80
tom-nachum <tom.nachum@gmail.com>
2022-11-09 13:51:02 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <p>Hi</p>
+      <p>Hello</p>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
