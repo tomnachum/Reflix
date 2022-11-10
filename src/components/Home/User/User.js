41a190160b507d29cd9d36b7c86c8c05a71a5f9b
tom-nachum <tom.nachum@gmail.com>
2022-11-10 12:10:19 +0200

    added user component for home screen

---
+++ src/components/Home/User/User.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./User.css";
+
+class User extends Component {
+  render() {
+    return (
+      <Link
+        exact
+        to="/catalog"
+        className="user"
+        style={{ backgroundColor: this.props.color }}
+      >
+        <div className="center">
+          <h2 className="center">{this.props.name}</h2>
+        </div>
+      </Link>
+    );
+  }
+}
+
+export default User;
