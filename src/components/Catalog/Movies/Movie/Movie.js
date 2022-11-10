a5a8a9c8dae3e3dbb0d3b93dd94f2b505ab2dd3c
tom-nachum <tom.nachum@gmail.com>
2022-11-10 17:29:27 +0200

    improved css

--- src/components/Catalog/Movies/Movie/Movie.js
+++ src/components/Catalog/Movies/Movie/Movie.js
@@ -4,7 +4,8 @@ import { FaPlusCircle, FaMinusCircle } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 class Movie extends Component {
-  changeStatus = () => {
+  changeStatus = e => {
+    e.preventDefault();
     this.props.changeMovieStatus(
       this.props.movie.id,
       !this.props.movie.isRented
@@ -14,15 +15,15 @@ class Movie extends Component {
   render() {
     return (
       <div className="movie center">
-        <div className="btn-container" onClick={this.changeStatus}>
-          {this.props.movie.isRented ? (
-            <FaMinusCircle className="btn" />
-          ) : (
-            <FaPlusCircle className="btn" />
-          )}
-        </div>
         <Link to={`/movies/${this.props.movie.id}`}>
           <div className="box">
+            <div className="btn-container" onClick={this.changeStatus}>
+              {this.props.movie.isRented ? (
+                <FaMinusCircle className="btn" />
+              ) : (
+                <FaPlusCircle className="btn" />
+              )}
+            </div>
             <img src={this.props.movie.img} alt="" />
           </div>
         </Link>
